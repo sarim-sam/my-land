769ba5b20233ebd5118c53c74086a06a941ace36
sarim <sarimslayerali786@gmail.com>
2024-11-20 20:49:59 +0500

    Design e'

--- src/Pages/MyTestimonials/myTestimonials.jsx
+++ src/Pages/MyTestimonials/myTestimonials.jsx
@@ -10,7 +10,7 @@ import arrowRight from '@iconify/icons-mdi/arrow-right';
 // TestimonialCard.
 const TestimonialCard = ({ name, title, image, icon, quote }) => {
   return (
-    <div className="group max-w-md p-8 m-6 rounded-[30px] shadow-lg bg-white relative transform transition-transform duration-300 hover:scale-105 hover:bg-[#684fff] hover:text-white active:scale-110 will-change-transform">
+    <div className="group 2xl:p-12 sxl:p-12 xl:p-12 lg:p-8 md:p-8 sm:p-12 xs:p-12 ls:p-8 ms:p-4 m-6 rounded-[30px] shadow-lg bg-white relative transform transition-transform duration-300 hover:scale-105 hover:bg-[#684fff] hover:text-white active:scale-110 will-change-transform">
       <div className="flex items-center gap-4 mb-4 relative">
         <div className="relative">
           <img
@@ -74,47 +74,38 @@ const MyTestimonials = () => {
     }
   };
   return (
-    <div
-      className="relative w-full min-h-[637px] bg-[#FFFFFF] overflow-hidden flex flex-col items-start justify-start mt-14">
-    <img
-      src="/myGroup.png"
-      alt="Background"
-      className="absolute inset-0 left-5 z-0 max-w-full max-h-[580px] hidden sm:hidden md:block"
-    />
+  <div className="relative w-full min-h-[637px] bg-[#FFFFFF] overflow-hidden flex flex-col items-start justify-start mt-14">
+      <img src="/myGroup.png" alt="Background"className="absolute inset-0 left-5 z-0 max-w-full max-h-[580px] hidden sm:hidden md:block"/>
 
-<div className='2xl:hidden xl:hidden lg:hidden md:hidden sm:block'>
-  <img src="/decorator-first.png"  className='absolute left-1/2 transform -translate-x-1/2'/>
-  <img src="/decorator-second.png" className='absolute bottom-[-8rem] left-1/2 transform -translate-x-1/2'/>
-  </div>
-
-  {/* <div className="flex items-center w-full mx-2 sm:mx-4 mb-8 mt-12">
-  <p className="text-5xl sm:text-3xl md:text-5xl poppins-bold mr-auto py-8"> */}
+    <div className='2xl:hidden xl:hidden lg:hidden md:hidden sm:block'>
+      <img src="/decorator-first.png"  className='absolute left-1/2 transform -translate-x-1/2'/>
+      <img src="/decorator-second.png" className='absolute bottom-[-8rem] left-1/2 transform -translate-x-1/2'/>
+    </div>
 
 
+    <div className="flex sxl:h-28 flex-col lg:flex-row md:flex-col sm:flex-col xs:flex-col ls:flex-col ms:flex-col items-center justify-around w-full mx-2 sm:mx-4 mb-8 z-10 2xl:-mx-16">
+      {/* Heading */}
+      <p className="poppins-bold text-center sm:text-left w-full sm:w-auto 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl ms:text-3xl mb-4 sm:mb-0">
+        What Our Client Said about us
+      </p>
 
-<div className="flex sxl:h-28 flex-col lg:flex-row md:flex-col sm:flex-col xs:flex-col ls:flex-col ms:flex-col items-center justify-around w-full mx-2 sm:mx-4 mb-8 z-10 2xl:-mx-16">
-  {/* Heading */}
-  <p className="poppins-bold text-center sm:text-left w-full sm:w-auto 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl ms:text-3xl mb-4 sm:mb-0">
-    What Our Client Said about us
-  </p>
+      {/* Navigation Buttons */}
+      <div className="flex gap-4 sm:gap-6">
+       {/* LEFT BUTTON */}
+       <button onClick={handlePrev} className="2xl:w-20 2xl:h-20 xl:w-20 xl:h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#F8F9FF] hover:bg-[#f4f4fa] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 mr-4">
+        <Icon icon={arrowLeft} width="40" height="40" className="text-[#836fff]" />
+       </button>
 
-  {/* Navigation Buttons */}
-  <div className="flex gap-4 sm:gap-6">
-    {/* LEFT BUTTON */}
-    <button onClick={handlePrev} className="2xl:w-20 2xl:h-20 xl:w-20 xl:h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#F8F9FF] hover:bg-[#f4f4fa] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 mr-4">
-      <Icon icon={arrowLeft} width="40" height="40" className="text-[#836fff]" />
-    </button>
-
-    {/* RIGHT BUTTON */}
-    <button onClick={handleNext} className="2xl:w-20 2xl:h-20 xl:w-20 xl:h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#684fff] hover:bg-[#836fff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 mr-4">
-      <Icon icon={arrowRight} width="40" height="40" className="text-[#ffffff]" />
-    </button>
-  </div>
-</div>
+       {/* RIGHT BUTTON */}
+       <button onClick={handleNext} className="2xl:w-20 2xl:h-20 xl:w-20 xl:h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#684fff] hover:bg-[#836fff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 mr-4">
+        <Icon icon={arrowRight} width="40" height="40" className="text-[#ffffff]" />
+      </button>
+     </div>
+    </div>
 
       {/* Testimonials */}
       <Testimonials startIndex={startIndex} direction={direction} />
-    </div>
+  </div>
   );
 };
 
